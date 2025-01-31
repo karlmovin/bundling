4d16ac5d889fd17c00b7bda2f7486389e316fb0f
Karl <40166249+kfmovin@users.noreply.github.com>
2025-01-31 14:08:55 +0100

    separate app.jsx

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-export default function App() {
-  return <div>Hello, webpack! </div>;
-}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<App />);
